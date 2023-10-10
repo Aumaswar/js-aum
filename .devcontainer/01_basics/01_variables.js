9f24b0d9b98e8f585abdfe6ccdf2c108e17e86ff
aum aswar <118254676+Aumaswar@users.noreply.github.com>
2023-10-10 15:59:22 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 283006
+let accountEmail = "aum.aswar06@gmail.com"
+var accountPassword = "7777"
+accountCity = "jamnagar"
+// accountId = 2 changing const is not allowed
+
+accountEmail = "aum@gmail.com"
+accountPassword = "1111"
+accountCity = 'baroda'
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
